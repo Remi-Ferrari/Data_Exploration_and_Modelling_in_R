e0dbb78af0eb36fa2a8d5b2d1c594bc164de7b12
Remi-Ferrari <remimaxime.ferrari+1@gmail.com>
2024-09-02 15:54:52 +0200

    Update Classification Moddel.r

--- Classification Moddel.r
+++ Classification Moddel.r
@@ -2,8 +2,26 @@
 Clasification Model
 '''
 
-set.seed(100)
- 
-TrainingIndex <- createDataPartition(iris$Species, p=0.8, list = FALSE)
-TrainingSet <- iris[TrainingIndex,] 
-TestingSet <- iris[-TrainingIndex,] 
+>set.seed(100)
+> 
+>TrainingIndex <- createDataPartition(iris$Species, p=0.8, list = FALSE)
+>TrainingSet <- iris[TrainingIndex,] 
+>TestingSet <- iris[-TrainingIndex,]
+>
+> Model <- train(Species ~ ., data = TrainingSet,
++                method = "svmPoly",
++                na.action = na.omit,
++                preProcess=c("scale","center"),
++                trControl= trainControl(method="none"),
++                tuneGrid = data.frame(degree=1,scale=1,C=1)
++ )
+> Model.cv <- train(Species ~ ., data = TrainingSet,
++                   method = "svmPoly",
++                   na.action = na.omit,
++                   preProcess=c("scale","center"),
++                   trControl= trainControl(method="cv", number=10),
++                   tuneGrid = data.frame(degree=1,scale=1,C=1)
++ )
+> Model.training <-predict(Model, TrainingSet)   
+> Model.testing <-predict(Model, TestingSet)    
+> Model.cv <-predict(Model.cv, TrainingSet) 
