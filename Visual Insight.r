e643b238966db014ce070137256ffeb5a8bc4f1c
Remi-Ferrari <remimaxime.ferrari+1@gmail.com>
2024-09-02 13:31:47 +0200

    Update Visual Insight.r

--- Visual Insight.r
+++ Visual Insight.r
@@ -2,4 +2,22 @@
 Visual insights of iris dataset
 '''
 
+#Panel Plots
 plot(iris)
+
+#Scatter Plot
+#Sepal Length vs Sepal Width
+#Colour can be added through ", col="xxx"" within plot arguments
+plot(iris$Petal.Width, iris$Petal.Length)
+
+#Histogram
+hist(iris$Sepal.Width)
+
+#Feature Plots
+#import caret library
+featurePlot(x = iris[,1:4], 
+             y = iris$Species,  
+             plot = "box",
+             strip=strip.custom(par.strip.text=list(cex=.7)),
+             scales = list(x = list(relation="free"), 
+                           y = list(relation="free")))
